8c2b4a5a55de99b845a83cda851dd39625d591f5
ziggypop <zimhen7@gmail.com>
2016-05-05 10:31:19 -0400

    I seem to have fixed the Lane formatting problem

--- Test/LaneTest.java
+++ Test/LaneTest.java
@@ -27,6 +27,8 @@ public class LaneTest {
         Assert.assertEquals(scores[0], frame1Roll1);
         Assert.assertEquals(scores[1], frame1Roll2);
 
+        Assert.assertEquals(frames.size(), 1);
+
     }
 
     @Test
@@ -35,6 +37,7 @@ public class LaneTest {
         for (int i = 0; i < 21; i++) {
             scores[i] = 0;
         }
+        scores[0] = 3;
         scores[18] = 10;
         scores[19] = 10;
         scores[20] = 5;
@@ -42,14 +45,30 @@ public class LaneTest {
 
         ArrayList<BowlingFrame> frames = Lane.formatScoresToFrames(scores);
 
-        int frameLastRoll1 = frames.get(10).getRoll1();
-        int frameLastRoll2 = frames.get(10).getRoll2();
-        int frameLastRoll3 = frames.get(10).getRoll3();
+        System.out.println("number of frames: " + frames.size());
 
+        int count = 0;
+        for (BowlingFrame frame : frames){
+
+            System.out.println("Frame "+ count +": " + frame.toString());
+            count++;
+        }
+
+        int frameFirstRoll1 = frames.get(0).getRoll1();
+
+        int frameLastRoll1 = frames.get(9).getRoll1();
+        int frameLastRoll2 = frames.get(9).getRoll2();
+        int frameLastRoll3 = frames.get(9).getRoll3();
+
+        Assert.assertEquals(scores[0], frameFirstRoll1);
         Assert.assertEquals(scores[18], frameLastRoll1);
         Assert.assertEquals(scores[19], frameLastRoll2);
         Assert.assertEquals(scores[20], frameLastRoll3);
 
+        Assert.assertEquals(frames.size(), 10);
+
     }
+
+
 
 }
